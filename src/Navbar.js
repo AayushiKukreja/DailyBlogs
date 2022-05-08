a20bc8e476afb2ad1d07a5b47521b8b76d6fd8f1
Aayushi <aayushikukreja@gmail.com>
2022-05-08 16:48:44 +0530

    Added new files

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom' ;
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+<h1>The Daily Blog</h1>
+<div className='links'>
+    <Link to='/'>Home</Link>
+    <Link to='/create' >
+    New Blog
+    </Link>
+</div>
+    </div>
+  )
+}
